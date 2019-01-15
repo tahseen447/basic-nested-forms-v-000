0a1e393fb91e053256c6002610285d945571fc06
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-15 22:15:15 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,7 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
-    @person.build.addresses(address_type: 'work')
+    @person.addresses.build(address_type: 'work')
     @person.build.address(address_type: 'home')
   end
 
